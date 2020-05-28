8c0051b9a48ce32910f2458643a92995713a2703
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-05-28 14:36:12 +0530

    segment tree
    to check it is balanced or not we have to sum all the values of brackets and if the result is 0 then we have to check if it is a balanced parathesis or not.

--- BRCKTS(spoj).cpp
+++ BRCKTS(spoj).cpp
@@ -1,54 +1,73 @@
-//This code is contributed by cacophonix
-#include <algorithm>
-#include <cstdio>
-
-#define NMAX 32768
-#define TMAX 65536
-
+#include<bits/stdc++.h>
 using namespace std;
+#define endl "\n"
+#define ll long long 
+ll arr[30005],n,Q;
 
-char seq[NMAX];
-struct Node { int sum, minsum; } Tree[TMAX];
+struct Node{
+    int sum,minsum;
+}st[120020];
 
-void init(int Node, int i, int j) {
-	if(i==j) {
-		Tree[Node].sum = Tree[Node].minsum = (seq[i]=='(' ? 1 : -1);
-		return;
-	}
-	int m = (i+j)/2;
-	init(2*Node, i, m);
-	init(2*Node+1, m+1, j);
-	Tree[Node].sum = Tree[2*Node].sum + Tree[2*Node+1].sum;
-	Tree[Node].minsum = min(Tree[2*Node].minsum, Tree[2*Node].sum + Tree[2*Node+1].minsum);
+void built(int si,int ss,int se){
+    if(ss==se){
+        st[si].sum=st[si].minsum=arr[ss];
+        return;
+    }
+    int mid=(ss+se)/2;
+    built(2*si,ss,mid);
+    built(2*si+1,mid+1,se);
+    st[si].sum=st[2*si].sum+st[2*si+1].sum;
+    st[si].minsum=min(st[2*si].minsum, st[2*si].sum+st[2*si+1].minsum);
 }
 
-void update(int Node, int i, int j, int idx) {
-	if(i==j) {
-		Tree[Node].sum = Tree[Node].minsum = -Tree[Node].sum;
-		return;
-	}
-	int m = (i+j)/2;
-	if(idx<=m) update(2*Node, i, m, idx);
-	else update(2*Node+1, m+1, j, idx);
-	Tree[Node].sum = Tree[2*Node].sum + Tree[2*Node+1].sum;
-	Tree[Node].minsum = min(Tree[2*Node].minsum, Tree[2*Node].sum + Tree[2*Node+1].minsum);
+void update(int si,int ss,int se,int idx){
+    if(ss==se){
+        arr[ss]=-arr[ss];
+        st[si].sum=st[si].minsum=arr[ss];
+        
+    }
+    else{
+        int mid=(ss+se)/2;
+        if(idx>=ss && idx<=mid) update(2*si,ss,mid,idx);
+        else update(2*si+1,mid+1,se,idx);
+        st[si].sum=st[2*si].sum+st[2*si+1].sum;
+        st[si].minsum=min(st[2*si].minsum, st[2*si].sum+st[2*si+1].minsum);
+    }
 }
 
-int main() {
-	int len, q, i, idx, test = 1;
-	while(scanf("%d", &len)==1) {
-		scanf("%s", seq);
-		init(1, 0, len-1);
-		printf("Test %d:\n", test++);
-		scanf("%d", &q);
-		for(i=0; i<q; i++) {
-			scanf("%d", &idx);
-			if(!idx) {
-				if(!Tree[1].sum && !Tree[1].minsum) printf("YES\n");
-				else printf("NO\n");
-			}
-			else update(1, 0, len-1, idx-1);
-		}
-	}
-	return 0;
+
+int main(){
+    int t=10,tr=1;
+    
+    while(t--){
+        scanf("%lld",&n);
+        ll j=1;
+        for(int i=0; i<n; i++){
+            char s;
+           scanf(" %c",&s);
+            if(s=='(') arr[j]=1;
+            else arr[j]=-1;
+            j++;
+        }
+        built(1,1,n);
+        scanf("%lld",&Q);
+        printf("Test %d:\n", tr++);
+        
+        while(Q--){
+            int idx;
+            scanf("%d",&idx);
+            
+            
+            if(idx==0){
+                
+                if(!st[1].sum && !st[1].minsum) printf("YES\n");
+                else printf("NO\n");
+            }
+            else{
+                update(1,1,n,idx);
+            }
+        }
+        
+    }
+    return 0;
 }
