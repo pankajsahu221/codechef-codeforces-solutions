c1658d58ec6e556bcbcbeefeea8d4b35529db83b
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-06-05 10:46:25 +0530

    BIT,map
    This problem is nothing but counting the no. of inversion(no. of smaller elements in the right side of an array element), which can be easily done using BIT and can also be done using segment tree but implementing BIT is more simple.

---
+++ YODANESS(spoj).cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+ll t,n,bit[30020],arr[30020];
+
+void update(ll idx,ll val){
+    
+    while(idx<=n){
+        bit[idx]+=val;
+        idx+=idx&(-idx);
+    }
+}
+
+ll getSum(ll idx){
+    ll sum=0;
+    
+    while(idx>0){
+        sum+=bit[idx];
+        idx-=idx&(-idx);
+    }
+    return sum;
+}
+
+int main(){
+    cin>>t;
+    
+    while(t--){
+        cin>>n;
+        
+        memset(bit,0,sizeof(bit));
+        
+        string s[n+9],temp;
+        map<string ,ll > m;
+        
+        for(ll i=1; i<=n; i++){
+             cin>>s[i];
+        }
+        for(ll i=1; i<=n; i++){
+            cin>>temp;
+            m[temp]=i;
+        }
+        for(ll i=1; i<=n; i++){
+            arr[i]=m[s[i]];
+        }
+        ll cntinv=0;
+        
+        for(ll i=n; i>0; i--){
+            cntinv+=getSum(arr[i]);
+            update(arr[i],1);
+        }
+         cout<<cntinv<<endl;
+        
+    }
+}
